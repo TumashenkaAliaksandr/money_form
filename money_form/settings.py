4478846797b339009bdd69bb0256ce0b15e26c91
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-09-01 14:10:05 +0300

    created .env and objects in settings.py with file .env

--- money_form/settings.py
+++ money_form/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,10 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STRIPE_SECRET_KEY = 'your_secret_key_here'
-STRIPE_PUBLIC_KEY = 'your_public_key_here'
+STRIPE_SECRET_KEY =  os.getenv('MAIN_STRIPE_SECRET_KEY')
+STRIPE_PUBLIC_KEY =  os.getenv('MAIN_STRIPE_PUBLIC_KEY')
 
-PAYPAL_CLIENT_ID = 'your_client_id_here'
-PAYPAL_SECRET_KEY = 'your_secret_key_here'
+PAYPAL_CLIENT_ID =  os.getenv('MAIN_PAYPAL_CLIENT_ID')
+PAYPAL_SECRET_KEY =  os.getenv('MAIN_PAYPAL_SECRET_KEY')
 
-BRAINTREE_SECRET_KEY = 'your_secret_key_here'
+BRAINTREE_SECRET_KEY =  os.getenv('MAIN_BRAINTREE_SECRET_KEY')
