56c5beb25671a602adc48c06963b4a6199725db8
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-09-01 13:50:13 +0300

    remuve file index in template/main, created view index

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 
 
+
 app_name = 'webapp'
 
 urlpatterns = [
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """money form"""
+    return render(request, 'main/index.html')
