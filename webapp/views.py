1fd2bc7adffe66b7f7fe0368fe9fcc70c9778a82
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-09-03 07:57:27 +0300

    added file forms.py and views

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,21 @@
+# from django.shortcuts import render
+#
+# def index(request):
+#     """money form"""
+#     return render(request, 'webapp/index.html')
+
 from django.shortcuts import render
+from .forms import PaymentForm
 
 def index(request):
-    """money form"""
-    return render(request, 'webapp/index.html')
+    if request.method == 'POST':
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            # Обработка данных формы
+            form.save()  # Сохранение данных в базе данных
+            # Добавьте здесь свою логику для обработки платежей и валидации карты
+
+    else:
+        form = PaymentForm()
+
+    return render(request, 'webapp/index.html', {'form': form})
